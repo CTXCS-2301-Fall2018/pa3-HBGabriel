e52e525152a760be5c93f576f538a41527f1f924
HBGabriel <skydoesminecraftbestfan@gmail.com>
2018-10-19 16:17:30 +0000

    Comleted PA3-2.s, segmentation error when exiting program but program works fine

--- PA3-2.s
+++ PA3-2.s
@@ -31,34 +31,35 @@ main:
     LDR R1, =select  @Address of var select
     LDR R1, [R1]     @Value of select in R1
 
-    @Your modifications will begin at this point
+	@Your modifications will begin at this point
 
-    CMP R1, #1       @Check for "peanuts"
-    BEQ _peanut      @If user entered 1 goto _peanuts
-    CMP R1, #2       @Check for "chocolate"
-    BEQ _choc        @If user entered 2 goto _choc
-    CMP R1, #3       @Check for "pretzels"
-    BEQ _pretzel     @If user entered 3 goto _pretzel
-    LDR R0, =msg7    @If we get here user entered
-                     @an illegal selection so print
-                     @error message and terminate
-    BL printf
-    MOV R7, #1
-    SWI #0           @Terminate, error condition
-_peanut:
-    MOV R3, #75      @Move 75 cents into R3
-    BAL _compute
-_choc:
-    MOV R3, #125     @Move 125 cents into R3
-    BAL _compute
-_pretzel:
-    MOV R3, #90      @Move 90 cents into R3
-_compute:
+	LDR R0, =msg6	@Final message
+	
+	CMP   R1, #1    @Check for "peanuts"
+	MOVEQ R3, #75   @If user entered 1 set R3 to 75
+	
+	LDRLT R0, =msg7
+	MOVLT R3, #1
+	BLLT  printf
+	MOVLT R7, #1
+	SWILT #0
+
+	CMP   R1, #2    @Check for "chocolate"
+	MOVEQ R3, #125  @If user entered 2 set R3 to 125
+
+	CMP   R1, #3    @Check for "pretzels"
+	MOVEQ R3, #90
+	MOVGT R3, #1
+			@If user entered 3 set R3 to 90
+	LDRGT R0, =msg7 @If we get here user entered
+        BLGT  printf 	@an illegal selection so print
+        MOVGT R7, #1 	@error message and terminate
+	SWIGT #0        @Terminate, error condition
+
     LDR R4, =quantity @Get address of var quantity
     LDR R4, [R4]      @Value of quantity now in R4
     MUL R1, R3, R4    @Multiply number of cents times quantity
                       @and put result in R1
-    LDR R0, =msg6     @Final message
     BL printf
     MOV R7, #0
     SWI #0        @Normal exit
